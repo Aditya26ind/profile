dd7942f4064e0e1f9c0c5492175208d7e120074c
adityasalabh <adi0699sal@gmail.com>
2021-09-22 15:47:52 +0530

    changes

--- myprofile/src/components/card.js
+++ myprofile/src/components/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./card.css"
 function Card(props){ 
     return(
-<div className="card ml-5 mt-5 ml-sm-5 bg-outline-primary"  id="card"style={{"width":"18rem" ,"height":"25rem"}}>
+<div className="card ml-5 mt-5 ml-sm-5 bg-outline-primary"  id="card"style={{"width":"18rem" ,"height":"23rem"}}>
   <img style={{"height":"10rem"}}src={props.image} className="card-img-top py-2 px-2" alt="..."/>
   <div className="card-body">
     <h5 className="card-title col-12 text-info">{props.pro_name}</h5>
